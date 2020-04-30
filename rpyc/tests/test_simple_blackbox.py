4ee3237dbd38001660353969116219a52c4ced9e
Ran Yeheskel <ran.yeheskel11@gmail.com>
2020-04-30 16:44:23 +0300

    Refactor simple blackbox test

--- rpyc/tests/test_simple_blackbox.py
+++ rpyc/tests/test_simple_blackbox.py
@@ -3,8 +3,11 @@ import builtins
 import pytest
 
 
+# gets a list of commands to the terminal (as strings)
+# Redirects these commands to the terminal as stdin
 @pytest.fixture()
 def set_input_commands(monkeypatch, request):
+    # objects of this class simulates stdin input to the terminal
     class InputCommandsFactory(object):
         def __init__(self, commands):
             self.call_number = 0
@@ -17,9 +20,12 @@ def set_input_commands(monkeypatch, request):
             self.call_number += 1
             return output
 
+    # patch builtin input method to the given input
     monkeypatch.setattr(builtins, "input", InputCommandsFactory(request.param))
 
 
+# receives the ip and port of the server
+# Returns a terminal connected to the server
 @pytest.fixture()
 def terminal(request):
     return rpyc_client.Terminal.Terminal(request.param[0], request.param[1])
@@ -31,5 +37,5 @@ def terminal(request):
 def test_simple_stupid_upload(capsys, terminal):
     terminal.start()
 
-    captured = capsys.readouterr()
-    assert len(captured.out) > 10 and 'p' in captured.out
+    captured = capsys.readouterr()  # the output of stdout, stderr
+    assert 'System Idle Process' in captured.out and captured.err == ''
