c72c993ad1859b4e79c12c773295f343abbe0fc1
Ran Yeheskel <ran.yeheskel11@gmail.com>
2020-05-03 10:30:19 +0300

    Support clean working directory for test purposes

--- rpyc/rpyc_slave/FileMonitorService.py
+++ rpyc/rpyc_slave/FileMonitorService.py
@@ -6,7 +6,30 @@ from threading import Thread
 
 # The fact that we inherit from rpyc.SlaveService makes the exposed_ prefix not to work.
 # i.e., if we'd inherit from rpyc.Service using the exposed_ prefix would have worked.
+import shutil
+
+
 class FileMonitorService(rpyc.SlaveService):
+    def __init__(self):
+        super().__init__()
+        self.__working_dir = "working_dir"
+        self.__connection_num = 0
+
+    def on_connect(self, conn):
+        if self.__connection_num == 0:
+            os.mkdir(self.__working_dir)
+            os.chdir(self.__working_dir)
+        self.__connection_num += 1
+
+        super().on_connect(conn)
+
+    def on_disconnect(self, conn):
+        super().on_disconnect(conn)
+        self.__connection_num -= 1
+        if self.__connection_num == 0:
+            os.chdir("..")
+            shutil.rmtree(self.__working_dir, ignore_errors=True)
+
     class FileMonitor(object):
         def __init__(self, filename, callback, interval=1):
             self.filename = filename
